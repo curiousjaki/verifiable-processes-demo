b6b0f8b9c383aa8f61d9ec8b3eacce1d5a747cb1
Jannis Kiesel <jaki@book.local>
2024-06-20 16:10:05 +0200

    added a carbon accounting risczero example

---
+++ risczero/carbon-accounting/methods/guest/src/main.rs
@@ -0,0 +1,22 @@
+#![no_main]
+// If you want to try std support, also update the guest Cargo.toml file
+#![no_std]  // std support is experimental
+
+use risc0_zkvm::guest::env;
+
+risc0_zkvm::guest::entry!(main);
+
+
+fn main() {
+    // TODO: Implement your guest code here
+
+    // read the input
+    let consumption: u32 = env::read();
+    let emission_factor: u32 = env::read();
+
+    // TODO: do something with the input
+    let co2_emissions: u32 = consumption * emission_factor;
+    // write public output to the journal
+    env::commit(&co2_emissions);
+    env::commit(&emission_factor);
+}
